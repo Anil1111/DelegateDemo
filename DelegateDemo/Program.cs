32183b1331023ebee4f5cd26007221e7fa38d5d8
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-02-23 18:24:10 +0530

    DelegateDemo

--- DelegateDemo/Program.cs
+++ DelegateDemo/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DelegateDemo
-{
+
     class Program
     {
+        delegate void mathfunc(); //creating a template for a method call, type for a method, treat methods more like data
+
         static void Main(string[] args)
         {
+            mathfunc method;
+
+
+            mathfunc[] mets = { Print, Write, Calc };
+            foreach (var i in mets)
+            {
+                i();
+            }
+        }
+
+        static void Print()
+        {
+            Console.WriteLine("Print");
+        }
+
+        static void Write()
+        {
+            Console.WriteLine("Write");
+        }
+
+        static void Calc()
+        {
+            Console.WriteLine("Calc");
         }
     }
-}
+
+
